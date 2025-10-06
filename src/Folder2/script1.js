d30fb68d528312e7071dc171b91a1aca0d833dd3
ElenaGQA <elena.grebeniuk@gmail.com>
2025-10-06 08:23:12 -0400

    sum function added

--- src/Folder2/script1.js
+++ src/Folder2/script1.js
@@ -24,11 +24,19 @@ function divide(a, b) {
     return a / b;
 }
 
+function sum (arr){
+let result = 0
+arr.forEach(el =>{
+result +=el
+})
+return result
+}
 // Test the calculator functions
 console.log("Add 10 + 5 =", add(10, 5));
 console.log("Subtract 10 - 5 =", subtract(10, 5));
 console.log("Multiply 10 * 5 =", multiply(10, 5));
 console.log("Divide 10 / 5 =", divide(10, 5));
+console.log("Array sum result is ", sum([1,2,3,4,5]))
 
 // Export functions for module usage (optional)
 module.exports = { add, subtract, multiply, divide };
