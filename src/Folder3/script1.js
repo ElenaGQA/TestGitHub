97f60ea2bbde9011f19d8f109ecacfc6982d8387
Elen <elen_grigoryan@epam.com>
2025-10-13 16:08:23 +0400

    created Folder3 with content script1.js

---
+++ src/Folder3/script1.js
@@ -0,0 +1,15 @@
+function greet(name){
+    return `Hello ${name}`
+}
+
+const greet=function(name){
+    return `Hello ${name}`
+}
+
+const greet=(name)=>{
+    return `Hello ${name}`
+}
+
+(function (name){
+    return `Hello ${name}`
+})()
